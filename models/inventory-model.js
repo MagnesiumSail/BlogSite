3a89aac13afad7e69d2be01b6a4092bd5552cb6a
MagnesiumSail <Leapster16@gmail.com>
2023-10-21 22:16:45 -0600

    fixed model function

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -27,10 +27,23 @@ async function getInventoryByClassificationId(classification_id) {
 }
 
 // inventoryModel.js
-exports.getVehicleById = async (id) => {
-  // Retrieve data from your database or API based on the given id.
-};
+async function getVehicleById(id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory WHERE inventory_id = $1`,
+      [id]
+    );
+    if (data.rows.length) {
+      return data.rows[0]; // assuming ID is unique, and you want to return the first matching row
+    } else {
+      return null; // or however you want to handle no results found
+    }
+  } catch (error) {
+    console.error("getVehicleById error: " + error);
+    throw error; // make sure you propagate the error so your controller or routes can handle it
+  }
+}
 
 
 
-module.exports = {getClassifications, getInventoryByClassificationId};
+module.exports = {getClassifications, getInventoryByClassificationId, getVehicleById};

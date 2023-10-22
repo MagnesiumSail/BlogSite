8f1168f6ae1236c4c0f297c683a5d0682430ab8e
MagnesiumSail <Leapster16@gmail.com>
2023-10-21 22:10:01 -0600

    fixed routing error

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,6 +6,7 @@ const invController = require("../controllers/invController")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 // inventoryRoute.js
-router.get('/vehicle/:id', inventoryController.getVehicleById);
+router.get('/inv/detail/:id', invController.getVehicleById);
+
 
 module.exports = router;

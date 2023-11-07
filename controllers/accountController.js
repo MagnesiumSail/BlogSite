0c741b2a67b7ce8849afda7b9ba632cf60bae320
MagnesiumSail <Leapster16@gmail.com>
2023-11-07 13:46:12 -0700

    fixed login issues

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,11 +5,16 @@ const utilities = require("../utilities/");
 *  Deliver login view
 * *************************************** */
 async function buildLogin(req, res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/login", {
-      title: "Login",
-      nav,
-    })
+  try {
+    console.log("buildLogin start");
+    let nav = await utilities.getNav();
+    console.log("Navigation data:", nav);
+    // ... rest of your original code ...
+  } catch (error) {
+    console.error("Error in buildLogin:", error);
+    next(error); // Make sure to forward the error to the error handler
   }
+}
+
   
   module.exports = { buildLogin }

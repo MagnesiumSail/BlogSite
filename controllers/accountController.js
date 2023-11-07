b8b84af6835619ca21acf95d93e9dc71d4767b25
MagnesiumSail <Leapster16@gmail.com>
2023-11-07 13:57:01 -0700

    started login css

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,16 +5,11 @@ const utilities = require("../utilities/");
 *  Deliver login view
 * *************************************** */
 async function buildLogin(req, res, next) {
-  try {
-    console.log("buildLogin start");
-    let nav = await utilities.getNav();
-    console.log("Navigation data:", nav);
-    // ... rest of your original code ...
-  } catch (error) {
-    console.error("Error in buildLogin:", error);
-    next(error); // Make sure to forward the error to the error handler
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      title: "Login",
+      nav,
+    })
   }
-}
-
   
   module.exports = { buildLogin }

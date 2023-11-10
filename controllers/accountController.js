c958d47f42d7dfdd0ded022fe90127cabcabda15
MagnesiumSail <Leapster16@gmail.com>
2023-11-10 15:54:07 -0700

    started prep

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,15 +1,24 @@
 // Import utilities module to utilize its functionalities.
 const utilities = require("../utilities/");
+const accountModel = require("../models/account-model");
 
 /* ****************************************
-*  Deliver login view
-* *************************************** */
+ *  Deliver login view
+ * *************************************** */
 async function buildLogin(req, res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/login", {
-      title: "Login",
-      nav,
-    })
-  }
-  
-  module.exports = { buildLogin }
+  let nav = await utilities.getNav();
+  res.render("account/login", {
+    title: "Login",
+    nav,
+  });
+}
+
+async function buildRegister(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/register", {
+    title: "Register",
+    nav,
+  })
+}
+
+module.exports = { buildLogin, buildRegister};
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,8 +4,10 @@ const router = new express.Router()
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController")
 
-// Route to build inventory by classification view
-router.get("/login", accountController.buildLogin);
+// Route to build inventory by account view
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+// Route to build register by register view
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
 
 
 module.exports = router;
